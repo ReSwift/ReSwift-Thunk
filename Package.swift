1cf592a4d6c17c4dd0767af516f61b16bd018649
Jay Hickey <hi@jayhickey.com>
2019-03-27 20:28:00 -0700

    Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+let package = Package(
+    name: "ReSwift-Thunk",
+    products: [
+      .executable(name: "ReSwift-Thunk", targets: ["ReSwift-Thunk"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/ReSwift/ReSwift", .upToNextMajor(from: "4.1.1"))
+    ],
+    targets: [
+      .target(
+        name: "ReSwift-Thunk",
+        dependencies: [
+          "ReSwift"
+        ], 
+        path: "ReSwift-Thunk"
+      )
+    ]
+)
