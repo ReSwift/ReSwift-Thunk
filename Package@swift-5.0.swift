fbeb6e55420a586cca09c5ff4985791d20975f37
Serge Rykovski <ioserge@yahoo.com>
2019-07-26 17:13:51 +0300

    Add support for swift package manager 5.0

---
+++ Package@swift-5.0.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ReSwift-Thunk",
+    products: [
+      .library(name: "ReSwift-Thunk", targets: ["ReSwift-Thunk"])
+    ],
+    dependencies: [
+      .package(url: "https://github.com/ReSwift/ReSwift", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+      .target(
+        name: "ReSwift-Thunk",
+        dependencies: [
+          "ReSwift"
+        ],
+        path: "ReSwift-Thunk"
+      )
+    ]
+)
